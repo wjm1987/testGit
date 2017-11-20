ebb5749fb42b3190292dd75488e77182f8c05685
wjm1987 <282729223@qq.com>
2017-11-20 14:36:44 +0800

    wwwwwwww

--- src/com/test/Test1.java
+++ src/com/test/Test1.java
@@ -2,6 +2,10 @@ package com.test;
 
 class Test1 {
 	public static void main(String[] args) {
-		System.out.println("31231");
+		System.out.println("312312222");
+		System.out.println("312312222");
+		System.out.println("312312222");
+		System.out.println("312312222");
+		System.out.println("312312222");
 	}
 }
